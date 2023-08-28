58421f2c9b196c3affd706569d45106b02cccddd
Javier-Villegas <javiervillegascarrasco@gmail.com>
2023-08-28 20:05:27 +0200

    fixes

--- rebuild_config.sh
+++ rebuild_config.sh
@@ -3,12 +3,14 @@
 # User config
 cd user_config
 directories=($(find . -type d | grep -v 'git'))
+directories+=("./")
 
 for dir in ${directories[@]}; do
 	if [ $dir != "." ]; then
 		mkdir -p $HOME/${dir:2}
 		files=$(ls -Ap $dir | grep -v '/')
 		for file in ${files[@]}; do
+			echo $(realpath $dir/$file) $HOME/${dir:2}/$file 
 			ln -sf $(realpath $dir/$file) $HOME/${dir:2}/$file
 		done
 	fi
@@ -23,8 +25,8 @@ for dir in ${directories[@]}; do
 	if [ $dir != "." ]; then
 		mkdir -p $HOME/${dir:2}
 		files=$(ls -Ap $dir | grep -v '/')
-		for file in ${files[@]}; do
-			ln -sf $(realpath $dir/$file) /${dir:2}/$file
-		done
-	fi
+ 		for file in ${files[@]}; do
+ 			ln -sf $(realpath $dir/$file) /${dir:2}/$file
+ 		done
+ 	fi
 done
